5327e427b89d624b9b2d1949e6908b4bd6e20d95
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2023-12-29 19:57:23 +1100

    added 10 questions in js

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+// List of all questions, choices, and answers
+
+var questions = [
+  {
+    title: '1. Which collection object allows unique value to be inserted only once?',
+    choices: ['Object', 'Set', 'Array', 'Map'],
+    answer: 'Set',
+  },
+  {
+    title: '2. Which method do you use to attach one DOM node to another?',
+    choices: ['attachNode()', 'getNode()', 'querySelector()', 'appendChild()'],
+    answer: 'appendChild()',
+  },
+  {
+    title: '3. Which statement can be used to skip an iteration in a loop?',
+    choices: ['break','skip','continue','pass'],
+    answer: 'continue',
+  },
+  {
+    title: '4. Which property references the DOM object that dispatched an event?',
+    choices: ['target', 'self', 'object', 'source'],
+    answer: 'target',
+  },
+  {
+    title: '5. Which method converts JSON data to a JavaScript object?',
+    choices: ['JSON.fromString()', 'JSON.parse()', 'JSON.toObject()', 'JSON.stringify()'],
+    answer: 'JSON.parse()',
+  },
+  {
+      title: '6. Which of the following methods can be used to display data in some form using Javascript?',
+      choices: ['document.write()', 'console.log()', 'window.alert()', 'All of the above'],
+      answer: 'All of the above',
+    },
+    {
+      title: '7. What does the Javascript "debugger" statement do?',
+      choices: ['It will debug all the errors in the program at runtime', 'It acts as a breakpoint in a program', 'It will debug error in the current statement if any', 'All of the above'],
+      answer: 'It acts as a breakpoint in a program',
+    },
+    {
+      title: '8. Which function is used to serialise an object into a JSON string in Javascript?',
+      choices: ['stringify()','parse()','convert()','None of the above'],
+      answer: 'stringify()',
+    },
+    {
+      title: '9. How do you stop an interval timer in Javascript?',
+      choices: ['clearTimer', 'clearInterval', 'intervalOver', 'None of the above'],
+      answer: 'clearInterval',
+    },
+    {
+      title: '10. The process in which an object or data structure is translated into a format suitable for transferral over a storage is called?',
+      choices: ['Object Inheritance', 'Object Encapsulation', 'Object Serialisation', 'None of the above'],
+      answer: 'Object Serialisation',
+    },
+];
